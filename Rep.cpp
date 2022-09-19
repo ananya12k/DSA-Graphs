cb1d4d10b4229590e35aef28a96a058b20f6bfd1
Ananya Kulkarni <ananyakulkarni1203@gmail.com>
2022-09-19 10:20:08 +0530

    Bfs traversal

--- Rep.cpp
+++ Rep.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<queue>
+#include <queue>
 using namespace std;
 int vertArr[20][20] = {0}; // the adjacency matrix initially 0
 int count = 0;
@@ -15,11 +15,27 @@ void displayMatrix(int v)
         cout << endl;
     }
 }
-int visit[2] = {0};
-void bfs(int v1)
+void bfs(int v1,int n)
 {
-
+    // visit[v1] = 1;
+    queue<int> Q;
+    // front = rear = -1;
+    Q.push(v1) ;
+    while (!Q.empty())
+    {
+        v1 = Q.front();
+        cout << v1 << endl;
+        for (int v2 = 0; v2 < n; v2++)
+        {
+            if (vertArr[v1][v2] == 1 && visit[v2] == 0)
+            {
+                Q.push(v2) ;
+                visit[v2] = 1;
+            }
+        }
+    }
 }
+int visit[2] = {0};
 void dfs(int v1, int n)
 {
     cout << v1 << endl;
@@ -39,11 +55,13 @@ void add_edge(int u, int v)
 }
 int main()
 {
-    int v = 2; // there are  2 vertices in the graph
+    int v = 3; // there are  3 vertices in the graph
     add_edge(0, 1);
     add_edge(1, 0);
-
+    add_edge(2, 1);
+    add_edge(1, 2);
     displayMatrix(v);
     dfs(1, v);
+    // bfs(1,v);
     return 0;
 }
